2b29faf48c523363fd80ae159f61c3cebc178330
Michael-Kochis <rhishisikk@gmail.com>
2021-07-05 12:00:52 -0500

    basic server now running locally with GET /

--- api/server.js
+++ api/server.js
@@ -1,3 +1,26 @@
 // BUILD YOUR SERVER HERE
+// imports
+const express = require('express');
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+// express app
+const server = express();
+
+// global middleware
+
+//endpoints
+// get helloWorld
+server.get("/", (req, res) => {
+    res.status(200).json({message: "Yip, Yip, Appa!"});
+})
+
+//get all users
+
+//get user by id
+
+//post new user
+
+//put update user by id
+
+//delete delete user by id
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
